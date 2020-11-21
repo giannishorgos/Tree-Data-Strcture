0491c06ca695bb777dd844c42a74f0c277d41521
giannishorgos <giannis.21.osfp@gmail.com>
2020-11-21 18:02:07 +0200

    Calculate the height

--- AvlTree.java
+++ AvlTree.java
@@ -1,5 +1,7 @@
 package com.java.btree;
 
+import com.java.btree.AvlNode;
+
 public class AvlTree {
     private AvlNode root;
 
@@ -11,28 +13,48 @@ public class AvlTree {
         AvlNode temp = root;
         boolean added = false;
         boolean addedRight = false;
+        int tempHeight = 0;
 
         while(!added) {
-            if(data >= temp.getData()) {
-                if(temp.getRight() != null) {
+            if (data >= temp.getData()) {
+                if (temp.getRight() != null) {
                     temp = temp.getRight();
-                }
-                else {
+                } else {
                     temp.setRight(new AvlNode(data));
                     addedRight = true;
                     added = true;
                 }
-            }
-            else {
-                if(temp.getLeft() != null) {
+            } else {
+                if (temp.getLeft() != null) {
                     temp = temp.getLeft();
-                }
-                else {
+                } else {
                     temp.setLeft(new AvlNode(data));
                     added = true;
                 }
             }
+            tempHeight++;
+        }
 
+        AvlNode finalNode = addedRight ? temp.getRight() : temp.getLeft();
+        temp = root;
+        while(temp != finalNode) {
+            if(finalNode.getData() >= temp.getData()) {
+                if(temp.getRightHeight() < tempHeight) {
+                    temp.setRightHeight(tempHeight);
+                }
+                temp = temp.getRight();
+            }
+            else {
+                if(temp.getLeftHeight() < tempHeight) {
+                    temp.setLeftHeight(tempHeight);
+                }
+                temp = temp.getLeft();
+            }
+            tempHeight--;
+        }
+        System.out.println(root.getRightHeight());
+/*
+        while
             AvlNode node = addedRight ? temp.getRight() : temp.getLeft();
             AvlNode tracker = root;
             temp = root;
@@ -64,7 +86,6 @@ public class AvlTree {
                     tracker = tracker.getLeft();
                 }
             }
-        }
-
+         */
     }
 }
